119fbcf0d4b2e35da7d91685c25246c8966d677e
Tobias <Tobias@DESKTOP-GQE1U3F>
2025-07-02 20:15:29 -0300

    carga inicial

---
+++ src/ar/edu/utn/fra/prog2/app/Parcial2.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ar.edu.utn.fra.prog2.app;
+
+/**
+ *
+ * @author Tobias
+ */
+public class Parcial2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
